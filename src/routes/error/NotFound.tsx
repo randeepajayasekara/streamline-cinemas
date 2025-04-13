b5da6a72a1a275306d060bc330afcb51c5d75825
Randeepa Jayasekara <147414352+randeepajayasekara@users.noreply.github.com>
2025-04-13 15:04:35 +0530

    Update: 404 UI Design

--- src/routes/error/NotFound.tsx
+++ src/routes/error/NotFound.tsx
@@ -1,18 +1,41 @@
-import { TextHoverEffect } from "@/components/animations/text-hover-effect"
-import { Button } from "@/components/ui/button"
+import { Spotlight } from "@/components/animations/spotlight-new";
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div className="bg-white dark:bg-black h-screen">
-      <div className="h-screen flex flex-col items-center justify-center">
-        <TextHoverEffect text="404" />
-        <Button variant="outline" className="text-black dark:text-white rounded-md">You have Reached an page that doesn't exist.</Button>
-      </div>
+    <div className="h-full w-full overflow-clip overflow-x-clip relative">
+      <Spotlight />
+      <main className="grid h-screen w-screen place-items-center bg-white dark:bg-black px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="text-base font-semibold text-amber-400">404</p>
+          <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-700 dark:text-gray-300 sm:text-7xl">
+            Page not found
+          </h1>
+          <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+            Sorry, we couldn’t find the page you’re looking for.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <Link
+              to="/"
+              className="rounded-md bg-amber-600 duration-200 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-amber-800 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Go back home
+            </Link>
+            <a
+              href="mailto:streamliecinemas@gmail.com"
+              className="text-sm font-semibold text-gray-900 dark:text-gray-400"
+            >
+              Contact support <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+        </div>
+      </main>
     </div>
-  )
-} 
+  );
+}
 
-{/*
+{
+  /*
     text,
     duration,
   }: {
@@ -36,4 +59,5 @@ export default function NotFound() {
         });
       }
     }, [cursor]);
-  */}
+  */
+}
